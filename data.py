d1c2ee246fc2ee2d67dd59b40b9f7796ac66f1bb
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-21 22:56:29 +0600

    settings

---
+++ data.py
@@ -0,0 +1,8 @@
+users = [
+    {
+        "id": "1",
+        "name": "aruuke",
+        "email": "aruuke@gmail.com",
+        "password": "qwerty"
+    }
+]
---
+++ query.py
@@ -0,0 +1,9 @@
+from graphene import ObjectType
+from data import users
+
+
+
+class Query(ObjectType):
+    def resolve_get_user(root, )
+
+
